509cbbb7925bf34a0675d2270675d49ef1756189
Utkarsh_190 <utkarshtyagi62@gmail.com>
2021-12-21 18:15:58 +0530

    created client side js for sending and recieving messages

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,28 @@
+const chatForm = document.getElementById("chat-form");
+const chatMessages = document.querySelector(".chat-messages");
+
 const socket = io();
+
+socket.on("message", (message) => {
+  console.log(message);
+  outputMessage(message);
+
+  // scroll down
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
+chatForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const msg = event.target.elements.msg.value;
+  socket.emit("chatMessage", msg);
+  event.target.elements.msg.value = "";
+  event.target.elements.msg.focus();
+});
+
+function outputMessage(message) {
+  const msgDiv = document.createElement("div");
+  msgDiv.classList.add("message");
+  msgDiv.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
+                   <p class="text">${message.text}</p>`;
+  document.querySelector(".chat-messages").appendChild(msgDiv);
+}
--- server.js
+++ server.js
@@ -2,19 +2,37 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
+const formatMessages = require("./utils/messages");
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-// set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
 // run when a client connects
 io.on("connection", (socket) => {
   console.log(`connection starting with ${socket.id}`);
-  socket.emit("message", "Welcome to chat app");
+  // for single connecting client
+  socket.emit("message", formatMessages("Admin", "Welcome to chat app"));
+
+  // broadcasts when a user connects
+  // for all except current connecting client
+  socket.broadcast.emit(
+    "message",
+    formatMessages("Admin", "a user joined chat")
+  );
+
+  // when user disconnects
+  socket.on("disconnect", () => {
+    // for all clients
+    io.emit("message", formatMessages("Admin", "A user has left chat"));
+  });
+
+  socket.on("chatMessage", (message) => {
+    io.emit("message", formatMessages("User", message));
+  });
 });
 
 server.listen(PORT, () => {
